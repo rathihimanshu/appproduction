6bd1383750f6c16240dc425e63b236ef33c197d1
himanshu <rathihimanshu@live.com>
2018-03-19 00:45:09 +0530

    fifth

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body bgcolor="#FFFF0F"><center>PRODUCTION</center>
 <br><br>
 hi
-<center><?php echo getenv("WEBSITE_HOSTNAME"); ?><center>
+<center>
+<?php echo "HI ALL" ?><center>
 </body>
 </html>
